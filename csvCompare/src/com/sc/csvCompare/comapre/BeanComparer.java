afd8b8a11812e3b8e06cfc7f978d66181d56f49f
ueec6tv <nikhilkuria@hotmail.com>
2014-03-26 15:36:12 +0530

    v 1.0 for test package

--- csvCompare/src/com/sc/csvCompare/comapre/BeanComparer.java
+++ csvCompare/src/com/sc/csvCompare/comapre/BeanComparer.java
@@ -3,7 +3,7 @@ package com.sc.csvCompare.comapre;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.apache.commons.collections.CollectionUtils;
+import org.apache.commons.collections4.CollectionUtils;
 
 import com.sc.csvCompare.bean.CompareOutput;
 import com.sc.csvCompare.bean.CsvBean;
@@ -38,7 +38,7 @@ public class BeanComparer {
 	private RowsCompliment fetchRowsCompliment(CsvBean expectedBean, CsvBean actualBean) {
 		RowsCompliment rowsCompliment = new RowsCompliment();
 		List<RowElement> extraRows = getMismatchRows(expectedBean,actualBean);
-		List<RowElement> missingRows = getMismatchRows(expectedBean,actualBean); 
+		List<RowElement> missingRows = getMismatchRows(actualBean,expectedBean); 
 		rowsCompliment.setExtraRows(extraRows);
 		rowsCompliment.setMissingRows(missingRows);
 		return rowsCompliment;
@@ -47,26 +47,31 @@ public class BeanComparer {
 	private List<RowElement> getMismatchRows(CsvBean expectedBean, CsvBean actualBean) {
 		List<RowElement> mismatchRows = new ArrayList<RowElement>();
 		
-		List<RowElement> actualRows = expectedBean.getRows();
+		List<RowElement> actualRows = actualBean.getRows();
 		List<RowElement> expectedRows = expectedBean.getRows();
 		
 		for (RowElement actualRowElement : actualRows ) {
 			List<String> actualRow = actualRowElement.getRowEntries();
+			boolean rowFound = false;
 			for (RowElement expectedRowElement : expectedRows) {
 				List<String> expectedRow = expectedRowElement.getRowEntries();
-				if(!CollectionUtils.disjunction(actualRow, expectedRow).isEmpty()){
-					RowElement mismatchRowElement = new RowElement();
-					mismatchRowElement.setRowEntries(actualRow);
-					mismatchRowElement.setRowHeaders(actualBean.getHeader());
-					mismatchRows.add(mismatchRowElement);
+				
+				if(CollectionUtils.disjunction(actualRow, expectedRow).isEmpty()){
+					rowFound = true;
+					break;
 				}
+			}
+			if(!rowFound){
+				RowElement mismatchRowElement = new RowElement();
+				mismatchRowElement.setRowEntries(actualRow);
+				mismatchRowElement.setRowHeaders(actualBean.getHeader());
+				mismatchRows.add(mismatchRowElement);
 			}
 		}
 		
 		return mismatchRows;
 	}
 
-	@SuppressWarnings("unchecked")
 	private HeadersCompliment validateHeaders(HeaderElement expectedHeader,
 			HeaderElement actualHeader) {
 		
@@ -77,10 +82,10 @@ public class BeanComparer {
 		headersCompliment.setMissingHeaders(new ArrayList<String>(
 				CollectionUtils.removeAll(expectedHeaders, actualHeaders)));
 		headersCompliment.setExtraHeaders(new ArrayList<String>(CollectionUtils
-				.retainAll(expectedHeaders,actualHeaders)));
+				.removeAll(actualHeaders,expectedHeaders)));
 		headersCompliment.setCommonHeaders(new ArrayList<String>(
 				CollectionUtils.intersection(expectedHeaders, actualHeaders)));
-		headersCompliment.setMissingHeaders(actualHeaders);
+		headersCompliment.setActualHeaders(actualHeaders);
 		headersCompliment.setExpectedHeaders(expectedHeaders);
 
 		return headersCompliment;
--- csvCompare/src/com/sc/test/csvCompare/main/CsvCompareTest.java
+++ csvCompare/src/com/sc/test/csvCompare/main/CsvCompareTest.java
@@ -3,23 +3,153 @@ package com.sc.test.csvCompare.main;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
+import junit.framework.Assert;
+
+import org.junit.Before;
 import org.junit.Test;
 
 import com.sc.csvCompare.bean.CompareOutput;
+import com.sc.csvCompare.bean.HeadersCompliment;
+import com.sc.csvCompare.bean.RowsCompliment;
 import com.sc.csvCompare.config.ConfigurationsManager;
 import com.sc.csvCompare.core.CsvComparer;
 
 public class CsvCompareTest {
 
-	@Test
-	public void compareTest() throws FileNotFoundException, IOException{
-		File expectedFile = new File("test-resources/expected-heading-1.txt");
-		File actualFile = new File("test-resources/actual-heading-1.txt");
-		File configFile = new File("test-resources/csv-config.properties");
+	File expectedFile;
+	File actualFile;
+	File configFile;
+	
+	@Before
+	public void initTest() throws FileNotFoundException, IOException{
+		configFile = new File("test-resources/csv-config.properties");
 		ConfigurationsManager.initializeManager(configFile);
+	}
+	
+	@Test
+	public void compareHeadersTest() throws FileNotFoundException, IOException{
+		expectedFile = new File("test-resources/expected-heading-1.txt");
 		CsvComparer comparer = new CsvComparer();
-		CompareOutput output = comparer.compareCsvFiles(expectedFile, actualFile);
+		List<String> commonHeaders = new ArrayList<String>();
+		List<String> missingHeaders = new ArrayList<String>();
+		List<String> extraHeaders = new ArrayList<String>();
+		CompareOutput output = null;
+		
+		actualFile = new File("test-resources/actual-heading-1.txt");
+		output = comparer.compareCsvFiles(expectedFile, actualFile);
+		
+		commonHeaders.add("heading1");
+		commonHeaders.add("heading2");
+		
+		missingHeaders.add("heading3");
+		missingHeaders.add("heading4");
+		
+		extraHeaders.add("headng3");
+		extraHeaders.add("headig4");
+		
+		HeadersCompliment headersCompliment = output.getHeadersCompliment();
+		Assert.assertEquals(headersCompliment.getCommonHeaders(), commonHeaders);
+		Assert.assertEquals(headersCompliment.getExtraHeaders(), extraHeaders);
+		Assert.assertEquals(headersCompliment.getMissingHeaders(), missingHeaders);
+		
+		actualFile = new File("test-resources/actual-heading-2.txt");
+		output = comparer.compareCsvFiles(expectedFile, actualFile);
+
+		commonHeaders.clear();
+		missingHeaders.clear();
+		extraHeaders.clear();
+		
+		commonHeaders.add("heading1");
+		commonHeaders.add("heading2");
+		commonHeaders.add("heading3");
+		commonHeaders.add("heading4");
+		
+		extraHeaders.add("heading5");
+		headersCompliment = output.getHeadersCompliment();
+		Assert.assertEquals(headersCompliment.getCommonHeaders(), commonHeaders);
+		Assert.assertEquals(headersCompliment.getExtraHeaders(), extraHeaders);
+		Assert.assertEquals(headersCompliment.getMissingHeaders(), missingHeaders);
+	}
+	
+	@Test
+	public void CompareRowsTest(){
+		expectedFile = new File("test-resources/expected-heading-1.txt");
+		actualFile = new File("test-resources/actual-content-1.txt");
+		CsvComparer comparer = new CsvComparer();
+		CompareOutput output = null;
+		
+		output = comparer.compareCsvFiles(expectedFile, actualFile);
+		
+		RowsCompliment rowsCompliment = output.getRowsCompliment();
+		
+		List<String> extraRowOne = new ArrayList<String>();
+		List<String> missingRowOne = new ArrayList<String>();
+		
+		List<String> extraRowTwo = new ArrayList<String>();
+		List<String> missingRowTwo = new ArrayList<String>();
+		
+		//one,two,thre,four
+		//ek,do,teen,char
+		extraRowOne.add("one");
+		extraRowOne.add("two");
+		extraRowOne.add("thre");
+		extraRowOne.add("four");
+		
+		extraRowTwo.add("ek");
+		extraRowTwo.add("do");
+		extraRowTwo.add("teen");
+		extraRowTwo.add("char");
+		
+		//one,two,thre,four
+		//ek,do,teen,char
+		missingRowOne.add("one");
+		missingRowOne.add("two");
+		missingRowOne.add("three");
+		missingRowOne.add("four");
+		
+		missingRowTwo.add("ek");
+		missingRowTwo.add("do");
+		missingRowTwo.add("theen");
+		missingRowTwo.add("char");
+		
+		Assert.assertEquals(rowsCompliment.getExtraRows().get(0).getRowEntries(), extraRowOne);
+		Assert.assertEquals(rowsCompliment.getMissingRows().get(0).getRowEntries(), missingRowOne);
+		
+		Assert.assertEquals(rowsCompliment.getExtraRows().get(1).getRowEntries(), extraRowTwo);
+		Assert.assertEquals(rowsCompliment.getMissingRows().get(1).getRowEntries(), missingRowTwo);
+		
+		actualFile = new File("test-resources/actual-content-2.txt");
+		output = comparer.compareCsvFiles(expectedFile, actualFile);
+		
+		rowsCompliment = output.getRowsCompliment();
+		
+		extraRowOne.clear();
+		extraRowTwo.clear();
+		
+		/*one,two,three,four,five
+		ek,do,theen,char,paanch
+		onnu,randu,moonu,nalu,anju*/
+		
+		extraRowOne.add("one");
+		extraRowOne.add("two");
+		extraRowOne.add("three");
+		extraRowOne.add("four");
+		extraRowOne.add("five");
+		
+		extraRowTwo.add("ek");
+		extraRowTwo.add("do");
+		extraRowTwo.add("theen");
+		extraRowTwo.add("char");
+		extraRowTwo.add("paanch");
+		
+		Assert.assertEquals(rowsCompliment.getExtraRows().get(0).getRowEntries(), extraRowOne);
+		Assert.assertEquals(rowsCompliment.getMissingRows().get(0).getRowEntries(), missingRowOne);
+		
+		Assert.assertEquals(rowsCompliment.getExtraRows().get(1).getRowEntries(), extraRowTwo);
+		Assert.assertEquals(rowsCompliment.getMissingRows().get(1).getRowEntries(), missingRowTwo);
 	}
 	
 }
